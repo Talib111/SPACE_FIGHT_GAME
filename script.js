e6ff4d29e586da587302eb701c4895db0b2ba485
Talib111 <talibplay111@gmail.com>
2021-09-17 16:32:38 +0530

    code opitmized

--- script.js
+++ script.js
@@ -5,50 +5,50 @@ let gameArea = document.getElementById("gameArea");
 let scoreElem = document.getElementById("score");
 let score = 0;
 var leftInterval, rightInterval;
+let moveInterval;
+let enemyWidth = 50;
+let enemyHeight = 50;
+let allenemyTop = 20;
+var fireInterval;
+let playButton;
+let loadingBackground;
+let fireSound = new Audio("./shoot.wav");
+
+
+///////// element constructor function//////////////////
+function elementCreator (elementType,style){
+    this.elem = document.createElement(elementType)
+    this.elem.style.cssText = `${style}`
+}
+/////////////// element constructor function/////////////
 
 //enemy constructor
-
 let enemyArray = ["50px", "150px", "250px", "350px", "450px", "550px"];
 let enemyContainer;
 const creatEnemy2 = () => {
     enemyArray.map((data, index) => {
-        enemyContainer = document.createElement("div");
-        let dynamicEnemy = document.createElement("img");
-        enemyContainer.id = "enemy" + index;
-        enemyContainer.style.cssText = `position: absolute;
-    width: 50px;
-    height: 50px;
-    left: ${data};
-    top: 20px;`;
+        enemyContainer = new elementCreator('div',`position: absolute;width: 50px;height: 50px;left: ${data};top: 20px;`)
+        enemyContainer.elem.id = "enemy" + index;
+        document.body.appendChild(enemyContainer.elem);
 
-        dynamicEnemy.src = "./enemy.svg";
-        dynamicEnemy.style.cssText = `width: 50px;
-    height: 50px`;
-
-        document.body.appendChild(enemyContainer);
-        enemyContainer.appendChild(dynamicEnemy);
+    let dynamicEnemy = new elementCreator('img',`width: 50px;height: 50px;`)
+        dynamicEnemy.elem.src = "./enemy.svg";
+        enemyContainer.elem.appendChild(dynamicEnemy.elem);
     });
 };
 
 creatEnemy2();
 
-const stop = () => {
-    clearInterval(leftInterval);
-    clearInterval(rightInterval);
-};
-
-let enemyWidth = 50;
-let enemyHeight = 50;
-let allenemyTop = 20;
 
 // checking blast
 const checkBlast = (missileTop, missileLeft) => {
+    //map for all enemy
     enemyArray.map((data, index) => {
         if (missileTop <= allenemyTop + enemyHeight)
             if (missileTop >= allenemyTop)
                 if (missileLeft <= parseInt(data) + enemyWidth)
                     if (missileLeft >= parseInt(data)) {
-                        document.body.removeChild(missile);
+                        document.body.removeChild(missile.elem);
                         score++;
                         scoreElem.innerHTML = score;
                         document.body.removeChild(document.getElementById("enemy" + index));
@@ -58,13 +58,11 @@ const checkBlast = (missileTop, missileLeft) => {
                     }
     });
 
-    //map from all enemy position
+    
 };
-// checking blast
-let moveInterval;
 
+//fighter movement
 function moveFighter(direction){
-
     clearInterval(moveInterval)
          moveInterval = setInterval(() => {
         let nextPosition
@@ -89,7 +87,6 @@ function moveFighter(direction){
 leftKey.addEventListener("touchstart", function () {
     moveFighter('left')
 });
-
 rightKey.addEventListener("touchstart", function () {
     moveFighter('right')
 });
@@ -101,25 +98,19 @@ leftKey.addEventListener("touchend", function () {
 });
 
 //creating missile
-let missile = document.createElement("div");
-missile.style.width = "5px";
-missile.style.height = "20px";
-missile.style.backgroundColor = "yellow";
-missile.style.position = "absolute";
-//*creating missile
+let missile = new elementCreator('div',`width: 5px;height: 20px;background-color: yellow;position: absolute;`)
 
-var fireInterval;
-let fireSound = new Audio("./shoot.wav");
-
+//activate missile function
 const activateMissile = () => {
     let fighterLeft = parseInt(
         window.getComputedStyle(fighter, null).getPropertyValue("left")
     );
     let missileLeftTarget = fighterLeft + 24;
-    missile.style.left = missileLeftTarget + "px";
-    missile.style.top = "300px";
-    document.body.appendChild(missile);
+    missile.elem.style.left = missileLeftTarget + "px";
+    missile.elem.style.top = "300px";
+    document.body.appendChild(missile.elem);
 };
+
 const fire = () => {
     //fire sound
     fireSound.currentTime = 0;
@@ -129,85 +120,52 @@ const fire = () => {
     clearInterval(fireInterval);
     fireInterval = setInterval(() => {
         let missileTop = parseInt(
-            window.getComputedStyle(missile, null).getPropertyValue("top")
+            window.getComputedStyle(missile.elem, null).getPropertyValue("top")
         );
         let missileLeft = parseInt(
-            window.getComputedStyle(missile, null).getPropertyValue("left")
+            window.getComputedStyle(missile.elem, null).getPropertyValue("left")
         );
 
         //checkblast call
         checkBlast(missileTop, missileLeft);
         if (missileTop == 0) {
-            //destroy the element
-            document.body.removeChild(missile);
+            //destroy the missile
+            document.body.removeChild(missile.elem);
             clearInterval(fireInterval);
         }
         let nextPosition = missileTop - 5;
-        missile.style.top = nextPosition + "px";
+        missile.elem.style.top = nextPosition + "px";
     }, -5);
 
-    // scoreElem.innerHTML = score++
 };
 
-let playButton;
-let loadingBackground;
+
 const splashScreen = () => {
-    loadingBackground = document.createElement("div");
-    loadingBackground.style.cssText = `
-  height: 100vh;
-  width: 100vw;
-  background-image: linear-gradient(180deg,black,red,white);
-  position: absolute;
-  left: 0;
-  top: 0;
-  z-index: 100000;
-  display: flex;
-  justify-content: center;
-  align-items: center;`;
-    document.body.appendChild(loadingBackground);
+    let loadbarWidth = 0;
+
+    //splash screen background
+    loadingBackground = new elementCreator('div',`height: 100vh;width: 100vw;background-image: linear-gradient(180deg,black,red,white);position: absolute;left: 0;top: 0;z-index: 100000;display: flex;justify-content: center;align-items: center;`)
+    document.body.appendChild(loadingBackground.elem)
 
     //text
-    let introText = document.createElement("h1");
-    introText.innerHTML = "Space Shooter";
-    introText.style.cssText = `
-  text-align: center;
-  position: absolute;
-  color: white;
-  top: 100px;`;
-    loadingBackground.appendChild(introText);
+    let introText = new elementCreator('h1',`text-align: center;position: absolute;color: white;top: 100px;`)
+    introText.elem.innerHTML = 'SPACE SHOOTER'
+    loadingBackground.elem.appendChild(introText.elem);
+
 
     //loadinbar div
-    let loadinbar = document.createElement("div");
-    loadinbar.style.cssText = `
-  width: 400px;
-  height: 30px;
-  border: 2px solid white;
-  position: absolute;
-  border-radius: 10px`;
-
-    loadingBackground.appendChild(loadinbar);
+    let loadinbar = new elementCreator('div',`width: 400px;height: 30px;border: 2px solid white;position: absolute;border-radius: 10px;`)
+    loadingBackground.elem.appendChild(loadinbar.elem);
 
     //inside loadinbar div
-    let loadinbarInside = document.createElement("div");
-    loadinbarInside.style.cssText = `
-height: 30px;
-position: absolute;
-background-image: linear-gradient(90deg,black,yellow);
-textAlign: center;
-color: white;
-display: flex;
-justify-content: center;
-align-items: center;
-border-radius: 10px
-`;
-    loadinbar.appendChild(loadinbarInside);
+    let loadinbarInside = new elementCreator('div',`height: 30px;position: absolute;background-image: linear-gradient(90deg,black,yellow);textAlign: center;color: white;display: flex;justify-content: center;align-items: center;border-radius: 10px;`)
+    loadinbar.elem.appendChild(loadinbarInside.elem);
 
-    let loadbarWidth = 0;
     let loadingInterval = setInterval(() => {
         console.log("working");
         loadbarWidth++;
-        loadinbarInside.style.width = loadbarWidth + "px";
-        loadinbarInside.innerHTML = loadbarWidth;
+        loadinbarInside.elem.style.width = loadbarWidth + "px";
+        loadinbarInside.elem.innerHTML = loadbarWidth;
         if (loadbarWidth == 400) {
             clearInterval(loadingInterval);
             //call to create play button
@@ -216,28 +174,18 @@ border-radius: 10px
     }, 5);
 
     //play button
-    playButton = document.createElement("button");
-    playButton.innerHTML = "PLAY";
-    playButton.style.cssText = `textAlign: center;
-color:black;
-position: relative;
-top: 70px;
-background-color: yellow;
-width: 120px;
-height: 45px;
-border-radius: 20px;
-font-weight: 800px;
-`;
+    playButton = new elementCreator('button',`textAlign: center;color:black;position: relative;top: 70px;background-color: yellow;width: 120px;height: 45px;border-radius: 20px;font-weight: 800px;`)
+    playButton.elem.innerHTML = 'PLAY'
 
     const createPlay = () => {
-        loadingBackground.appendChild(playButton);
+        loadingBackground.elem.appendChild(playButton.elem);
     };
 };
 
 splashScreen();
 
 const removeSplash = () => {
-    document.body.removeChild(loadingBackground);
+    document.body.removeChild(loadingBackground.elem);
     document.body
         .requestFullscreen()
         .then(function () {
@@ -250,10 +198,7 @@ const removeSplash = () => {
 };
 
 //play button call
-playButton.addEventListener("click", function () {
+playButton["elem"].addEventListener("click", function () {
     removeSplash();
 });
 
-/////////  LEARN LOGIC ////////
-
-//==== 1 BLAST CASE -----  missile position is equal to enemy position
